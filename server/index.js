4b27131e77873ba18418919e8e7c2bac49c75401
Tandid Alam <tandid.alam@gmail.com>
2022-10-17 15:52:27 -0400

    Connected to MongoDB and added .env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
+const dotenv = require("dotenv").config();
+
+console.log(dotenv.parsed);
+require("./database");
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
