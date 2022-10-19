ead9a4d815154ef2afb3669757482702eabd68ff
Tandid Alam <tandid.alam@gmail.com>
2022-10-18 23:20:22 -0400

    Fixed issue with Joi

--- server/models/user.js
+++ server/models/user.js
@@ -27,15 +27,11 @@ const User = mongoose.model(
   })
 );
 
-function validateUser(user) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required(),
-  };
-
-  return Joi.validate(user, schema);
-}
+const schema = Joi.object({
+  name: Joi.string().min(5).max(50).required(),
+  email: Joi.string().min(5).max(255).required().email(),
+  password: Joi.string().min(5).max(255).required(),
+});
 
 exports.User = User;
-exports.validate = validateUser;
+exports.userSchema = schema;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,10 @@
-const { User } = require("../models/user");
+const { User, userSchema } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
-const { RestartProcess } = require("concurrently");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
+  const { error } = userSchema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
