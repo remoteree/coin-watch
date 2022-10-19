590dd678584ec84e1425bc21a34ca4055e01f48b
Tandid Alam <tandid.alam@gmail.com>
2022-10-19 15:53:07 -0400

    added winston for some more error handling

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 require("express-async-errors"); //Helps wrap routes to async error handling middleware
+const winston = require("winston"); //Helps with logging errors using Transport
+require("winston-mongodb");
 const error = require("./middleware/error");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+winston.handleExceptions(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" }) //Deals with handling exceptions and rejections
+);
+
+process.on("unhandledRejection", (ex) => {
+  throw ex;
+});
+
+winston.add(winston.transports.File, { filename: "logfile.log" }); //Adds error logs to this logfile
+winston.add(winston.transports.MongoDB, { db: process.env.MONGODB_URI }); //Adds error logs to mongoDB, normally we don't store this in database
+
+//---
 
 if (!process.env.jwtPrivateKey) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,6 +1,15 @@
 //middleware for catching errors
+const winston = require("winston");
 
 function error(err, req, res, next) {
+  winston.error(err.message, err);
+  //error
+  //warning
+  //info
+  //verbose
+  //debug
+  //silly
+
   res.status(500).send("Something failed");
 }
 
